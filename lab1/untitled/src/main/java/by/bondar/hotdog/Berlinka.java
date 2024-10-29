c45970fc5cc54866ff0948ee5e00d26a5cad0a69
Илья <ulmie6@gmail.com>
2024-10-29 21:46:28 +0300

    Рефакторинг кода и добавление класса-репозитория

---
+++ lab1/untitled/src/main/java/by/bondar/hotdog/Berlinka.java
@@ -0,0 +1,10 @@
+package by.bondar.hotdog;
+
+/**
+ * Класс Berlinka, наследующий HotDog.
+ */
+public class Berlinka extends HotDog {
+    public Berlinka() {
+        super("Berlinka", 4.5);
+    }
+}
---
+++ lab1/untitled/src/main/java/by/bondar/hotdog/HunterDog.java
@@ -0,0 +1,10 @@
+package by.bondar.hotdog;
+
+/**
+ * Класс HunterDog, наследующий HotDog.
+ */
+public class HunterDog extends HotDog {
+    public HunterDog() {
+        super("Hunter Dog", 5.0);
+    }
+}
